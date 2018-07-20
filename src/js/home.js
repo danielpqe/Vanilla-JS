bef239422e93e3982b881a00078b0e1958df8adb
danielpqe <danielpqe@gmail.com>
2018-07-20 15:45:13 -0500

    DataSet

--- src/js/home.js
+++ src/js/home.js
@@ -1,72 +1,5 @@
-// console.log('hola mundo!');
-// const noCambia = "Leonidas";
-//
-// let cambia = "@LeonidasEsteban"
-//
-// function cambiarNombre(nuevoNombre) {
-//     cambia = nuevoNombre
-// }
-//
-// const getUserAll = new Promise(function(todoBien, todoMal) {
-//     // llamar a un api
-//     setTimeout(function() {
-//         // luego de 3 segundos
-//         todoBien('se acabó el tiempo');
-//     }, 5000)
-// })
-//
-// const getUser = new Promise(function(todoBien, todoMal) {
-//     // llamar a un api
-//     setTimeout(function() {
-//         // luego de 3 segundos
-//         todoBien('se acabó el tiempo 3');
-//     }, 3000)
-// })
-//
-// // getUser
-// //   .then(function() {
-// //     console.log('todo está bien en la vida')
-// //   })
-// //   .catch(function(message) {
-// //     console.log(message)
-// //   })
-//
-// Promise.race([
-//     getUser,
-//     getUserAll,
-// ])
-//     .then(function(message) {
-//         console.log(message);
-//     })
-//     .catch(function(message) {
-//         console.log(message)
-//     })
-//
-//
-//
-// $.ajax('https://randomuser.me/api/sdfdsfdsfs', {
-//     method: 'GET',
-//     success: function(data) {
-//         console.log(data)
-//     },
-//     error: function(error) {
-//         console.log(error)
-//     }
-// })
-//
-// fetch('https://randomuser.me/api/dsfdsfsd')
-//     .then(function (response) {
-//         // console.log(response)
-//         return response.json()
-//     })
-//     .then(function (user) {
-//         console.log('user', user.results[0].name.first)
-//     })
-//     .catch(function() {
-//         console.log('algo falló')
-//     });
-
 (async function load() {
+    // await
     // action
     // terror
     // animation
@@ -74,57 +7,64 @@
         const response = await fetch(url);
         const data = await response.json()
         return data;
-
     }
     const $form = document.getElementById('form');
-    const $featuringContainer = document.getElementById('featuring');
     const $home = document.getElementById('home');
+    const $featuringContainer = document.getElementById('featuring');
 
-    // console.log(actionList, dramaList, animationList)
-    function setAttributes($element,attributes){
+
+    function setAttributes($element, attributes) {
         for (const attribute in attributes) {
-            $element.setAttribute(attribute,attributes[attribute])
+            $element.setAttribute(attribute, attributes[attribute]);
         }
     }
-    const BASE_API='https://yts.am/api/v2/';
+    const BASE_API = 'https://yts.am/api/v2/';
 
-    function featuringTemplate(peli){
-        return(`<div class="featuring">
-            <div class="featuring-image">
-                <img src="${peli.medium_cover_image}" width="70" height="100" alt="">
-            </div>
-            <div class="featuring-content">
-                <p class="featuring-title">Pelicula encontrada</p>
-                <p class="featuring-album">${peli.title}</p>
-            </div>
-        </div>`)
+    function featuringTemplate(peli) {
+        return (
+            `
+      <div class="featuring">
+        <div class="featuring-image">
+          <img src="${peli.medium_cover_image}" width="70" height="100" alt="">
+        </div>
+        <div class="featuring-content">
+          <p class="featuring-title">Pelicula encontrada</p>
+          <p class="featuring-album">${peli.title}</p>
+        </div>
+      </div>
+      `
+        )
     }
 
-    $form.addEventListener('submit',async (event)=>{
+    $form.addEventListener('submit', async (event) => {
         event.preventDefault();
-
-        $home.classList.add('search-active');
+        $home.classList.add('search-active')
         const $loader = document.createElement('img');
-        setAttributes($loader,{
-            src:'src/images/loader.gif',
-            height:50,
-            width:50,
+        setAttributes($loader, {
+            src: 'src/images/loader.gif',
+            height: 50,
+            width: 50,
         })
         $featuringContainer.append($loader);
-        const data = new FormData($form);
-        const peli = await getData(`${BASE_API}list_movies.json?limit=1&query_term=${data.get('name')}`);
-        const htmlString = featuringTemplate(peli.data.movies[0]);
-        $featuringContainer.innerHTML=htmlString;
 
-        });
+        const data = new FormData($form);
+        const {
+            data: {
+                movies: pelis
+            }
+        } = await getData(`${BASE_API}list_movies.json?limit=1&query_term=${data.get('name')}`)
+
+        const HTMLString = featuringTemplate(pelis[0]);
+        $featuringContainer.innerHTML = HTMLString;
+    })
 
     const actionList = await getData(`${BASE_API}list_movies.json?genre=action`)
     const dramaList = await getData(`${BASE_API}list_movies.json?genre=drama`)
     const animationList = await getData(`${BASE_API}list_movies.json?genre=animation`)
-
-    function videoItemTemplate(movie) {
+    console.log(actionList, dramaList, animationList)
+    function videoItemTemplate(movie, category) {
         return (
-            `<div class="primaryPlaylistItem">
+            `<div class="primaryPlaylistItem" data-id="${movie.id}" data-category=${category}>
         <div class="primaryPlaylistItem-image">
           <img src="${movie.medium_cover_image}">
         </div>
@@ -133,41 +73,36 @@
         </h4>
       </div>`
         )
-
     }
     function createTemplate(HTMLString) {
         const html = document.implementation.createHTMLDocument();
         html.body.innerHTML = HTMLString;
         return html.body.children[0];
-
     }
-    function addEventClick($elemento) {
-        $elemento.addEventListener('click',()=> {
-        showModal();
+    function addEventClick($element) {
+        $element.addEventListener('click', () => {
+            // alert('click')
+            showModal($element)
         })
-
     }
-    function renderMovieList(list, $container) {
+    function renderMovieList(list, $container, category) {
         // actionList.data.movies
         $container.children[0].remove();
         list.forEach((movie) => {
-            const HTMLString = videoItemTemplate(movie);
+            const HTMLString = videoItemTemplate(movie, category);
             const movieElement = createTemplate(HTMLString);
             $container.append(movieElement);
             addEventClick(movieElement);
         })
-
     }
     const $actionContainer = document.querySelector('#action');
+    renderMovieList(actionList.data.movies, $actionContainer, 'action');
 
-    renderMovieList(actionList.data.movies, $actionContainer);
     const $dramaContainer = document.getElementById('drama');
+    renderMovieList(dramaList.data.movies, $dramaContainer, 'drama');
 
-    renderMovieList(dramaList.data.movies, $dramaContainer);
     const $animationContainer = document.getElementById('animation');
-
-    renderMovieList(animationList.data.movies, $animationContainer);
-
+    renderMovieList(animationList.data.movies, $animationContainer, 'animation');
     // const $home = $('.home .list #item');
     const $modal = document.getElementById('modal');
     const $overlay = document.getElementById('overlay');
@@ -177,17 +112,18 @@
     const $modalImage = $modal.querySelector('img');
     const $modalDescription = $modal.querySelector('p');
 
-    function showModal() {
-        // Agregar clase a la lista de clases del elemento selector
+    function showModal($element) {
         $overlay.classList.add('active');
-        $modal.style.animation='modalIn .8s forwards';
+        $modal.style.animation = 'modalIn .8s forwards';
+        const id = $element.dataset.id;
+        const category = $element.dataset.category;
+
     }
 
-    $hideModal.addEventListener('click',hideModal);
+    $hideModal.addEventListener('click', hideModal);
     function hideModal() {
         $overlay.classList.remove('active');
-        $modal.style.animation='modalOut .8s forwards';
-    }
+        $modal.style.animation = 'modalOut .8s forwards';
 
+    }
 })()
-// await
